471cabf1ec05f7f3298bf7043e2e297069313104
Shilpa k <shilpasiva1510@gmail.com>
2019-03-20 11:24:06 +0530

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "users",controllers: {
+        registrations: 'users/registrations'
+      }
   root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_154527) do
+ActiveRecord::Schema.define(version: 2019_03_20_053503) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
